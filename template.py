f89dce427810f966da5b7ef476759f0044cc7e87
MrNote11 <aliyahsulaiman3@gmail.com>
2024-07-06 03:08:23 +0100

    folder structure added

--- template.py
+++ template.py
@@ -3,39 +3,39 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
 project_name = "cnnCloud"
 
-list_of_files = [".github/workflows/.gitkeep", 
-                 f"src/{project_name}/__init__.py",
-                 f"src/{project_name}/componets/__init__.py",
-                 f"src/{project_name}/utils/__init__.py",
-                 f"src/{project_name}/config/__init__.py",
-                 f"src/{project_name}/config/configuration.py",
-                 f"src/{project_name}/pipeline/__init__.py",
-                 f"src/{project_name}/entity/__init__.py",
-                 f"src/{project_name}/constants/__init__.py",
-                 "config/config.yaml",
-                 "dvc.ymal",
-                 "params.ymal"
-                 "requirements.txt",
-                 "setup.py",
-                 "research/trials.ipynb"
-                 ]
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/componets/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.ymal",
+    "params.ymal" "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
 
-for file_paths in list_of_files:
-    file_paths = Path(file_paths)
-    filedir, filename = os.path.split(file_paths)
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    filedir, filename = os.path.split(file_path)
     
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"creating directory; {filedir} for the file: {filename}")
+        logging.info(f"file_dir: {filedir}, file{filename}")
         
-    if (not os.path.exists(file_paths)) or (os.path.getsize(file_paths) == 0):
-        with open(file_paths, "w") as f:
+    if (not os.path.exists(file_path)) or os.path.getsize(file_path) == 0:
+        with open(file_path, "w") as f:
             pass
-            logging.info(f"Creating empty file: {file_paths}")
-            
+        logging.info(f"Creating empty file_path: {file_path}")
+        
     else:
-        logging.info(f"{filename} is  already Created;")            
+        logging.info(f"{filename} exist")    
